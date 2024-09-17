12377780414564bc4b87e54f0e586732e0833de1
Trajche Nakov <trajce.nakov@gmail.com>
2024-09-17 20:41:42 +0200

    Sample routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/test', function () {
+//   return 'Testing';
+//});
+
+//Route::get('/test', function () {
+//   return [
+//       [
+//           'id' => 1,
+//           'title' => 'Conference 2024'
+//       ],
+//       [
+//           'id' => 2,
+//           'title' => 'Conference 2025'
+//       ]
+//   ];
+//});
