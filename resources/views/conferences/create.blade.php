d8ca29c22977eb614a07740eb6a00b45e426be10
Trajche Nakov <trajce.nakov@gmail.com>
2024-09-17 21:28:13 +0200

    Additional fixes

--- resources/views/conferences/create.blade.php
+++ resources/views/conferences/create.blade.php
@@ -18,7 +18,8 @@
                                     <div class="col-span-full">
                                         <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
                                         <div class="mt-2">
-                                            <input type="text" name="title" id="title" autocomplete="street-address" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                            <input type="text" name="title" id="title"
+                                                   autocomplete="title" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                         </div>
                                     </div>
                                 </div>
@@ -26,7 +27,6 @@
                         </div>
 
                         <div class="mt-6 flex items-center justify-end gap-x-6">
-                            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
                             <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
                         </div>
                     </form>
