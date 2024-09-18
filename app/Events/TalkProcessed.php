ea53847cb1029a4a95269176d0fdc51cd350d8a2
Trajche Nakov <trajce.nakov@gmail.com>
2024-09-18 13:48:04 +0200

    Laravel reverb and Echo

---
+++ app/Events/TalkProcessed.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Talk;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TalkProcessed implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public Talk $talk)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('talk-processed.' . $this->talk->id),
+        ];
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,8 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+Echo.private('talk-processed.1')
+    .listen('TalkProcessed', (e) => {
+   console.log(e.talk);
+});
