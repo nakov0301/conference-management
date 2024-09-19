6fb58faa39b2262bfa83b8d88f4ae1ee8ac36513
Trajche Nakov <trajce.nakov@gmail.com>
2024-09-19 07:47:59 +0200

    Example of running commands

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+\Illuminate\Support\Facades\Schedule::command('app:notify-for-unreviewed-talks')
+    ->everyFiveSeconds();
